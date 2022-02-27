621023157ec060e061d45fa200f41a8aef58a37c
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-27 12:53:18 -0600

    feat: support environment variables using ts-dotenv

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,11 +1,8 @@
-import 'dotenv/config'
+import { load } from 'ts-dotenv'
 
-const environmentVariables = [ 'NODE_ENV' ] as const
-type Env = typeof environmentVariables[ number ]
-
-export const env = {} as Record<Env, string>
-for ( const key of environmentVariables ) {
-	const value = process.env[ key ]
-	if ( !value ) throw new Error( `Missing environment variable: ${ key }` )
-	env[ key ] = value
-}
+export const env = load( {
+	NODE_ENV: [
+		'development' as const,
+		'production' as const
+	]
+} )
