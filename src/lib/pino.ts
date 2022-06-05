663a516744e0595e3c7764d50eab17775b33dff1
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-05 12:09:31 -0600

    feat: log to file

--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -2,7 +2,7 @@ import { magentaBright } from 'colorette'
 import Pino from 'pino'
 import pretty from 'pino-pretty'
 
-export const pino = Pino( pretty( {
+const prettyStream = pretty( {
 	colorize: true,
 	customPrettifiers: {
 		time: ( ts: string | object ) => magentaBright(
@@ -10,5 +10,16 @@ export const pino = Pino( pretty( {
 				? new Date( ts ).toISOString()
 				: new Date().toISOString() )
 	},
-	ignore: 'pid,hostname'
-} ) )
+	ignore: 'pid,hostname',
+	mkdir: true
+} )
+
+export const pino = Pino(
+	{
+		timestamp: Pino.stdTimeFunctions.isoTime
+	},
+	Pino.multistream( [
+		{ stream: prettyStream },
+		{ stream: Pino.destination( 'pino.log' ) }
+	] )
+)
