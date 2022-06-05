a7f006dcb2abad79131e8b1e8c807ad1c8ff4f02
bitomic <35199700+bitomic@users.noreply.github.com>
2022-06-05 11:27:21 -0600

    feat: wiki queue

---
+++ src/queues/Wiki.ts
@@ -0,0 +1,21 @@
+import { Queue, QueueEvents, QueueScheduler } from 'bullmq'
+import { redis } from '../lib'
+
+export const WikiName = 'Wiki'
+
+export const WikiScheduler = new QueueScheduler( WikiName, { connection: redis } )
+
+export const WikiEvents = new QueueEvents( WikiName, { connection: redis } )
+
+export const WikiQueue = new Queue( WikiName, {
+	connection: redis,
+	defaultJobOptions: {
+		attempts: 3,
+		backoff: {
+			delay: 1000,
+			type: 'exponential'
+		},
+		removeOnComplete: true
+	}
+} )
+
