97b74c80e0efc650a63ddf1e501cefe33712be72
bitomic <35199700+bitomic@users.noreply.github.com>
2023-05-02 15:55:15 -0600

    feat: hsr rarities

---
+++ src/tasks/es.honkai-star-rail/Rarities.ts
@@ -0,0 +1,51 @@
+import { ApplyOptions } from '@sapphire/decorators'
+import { Fandom } from 'mw.js'
+import type { FandomWiki } from 'mw.js'
+import { format } from 'lua-json'
+import { HOUR } from '../../util'
+import type { JobsOptions } from 'bullmq'
+import type { PieceOptions } from '@sapphire/pieces'
+import { Task } from '../../framework'
+
+@ApplyOptions<PieceOptions>( {
+	name: 'es.honkai-star-rail/rarities'
+} )
+export class UserTask extends Task {
+	public override jobOptions: JobsOptions = {
+		repeat: {
+			every: HOUR
+		}
+	}
+
+	public async run(): Promise<void> {
+		const wiki = Fandom.getWiki( 'es.honkai-star-rail' )
+		const bot = await Task.getFandomBot( wiki )
+		const itemTypes = [ 'Personajes', 'Recursos' ]
+		const rarities: Record<string, number> = {}
+		for ( const itemType of itemTypes ) {
+			for ( let i = 1; i <= 5; i++ ) {
+				const category = `${ itemType } de ${ i } estrella${ i === 1 ? '' : 's' }`
+				const pages = await this.getPagesInCategory( wiki, category )
+				for ( const page of pages ) {
+					rarities[ page ] = i
+				}
+			}
+		}
+
+		this.logger.info( 'Updating rarities...' )
+		await bot.edit( {
+			bot: true,
+			text: format( rarities ),
+			title: 'Module:Rarezas/datos'
+		} )
+	}
+
+	protected async getPagesInCategory( wiki: FandomWiki, category: string ): Promise<string[]> {
+		return ( await wiki.queryList( {
+			cmlimit: 'max',
+			cmnamespace: 0,
+			cmtitle: `Category:${ category }`,
+			list: 'categorymembers'
+		} ) ).map( i => i.title )
+	}
+}
