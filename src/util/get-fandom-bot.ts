0b3fa6ebfd8a60d139cecbc5dd198e7013785e69
bitomic <35199700+bitomic@users.noreply.github.com>
2023-05-03 21:33:02 -0600

    fix: remove unused utility

--- src/util/get-fandom-bot.ts
+++
@@ -1,8 +0,0 @@
-import { env } from '../lib'
-import { Fandom } from 'mw.js'
-import type { FandomBot } from 'mw.js'
-
-export const getFandomBot = (): Promise<FandomBot> => new Fandom().login( {
-	password: env.FANDOM_PASS,
-	username: env.FANDOM_USER
-} )
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,1 @@
-export * from './get-fandom-bot'
 export * from './time'
