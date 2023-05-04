8b84a3606ca860dc81ecb81c789847efdb98c3af
bitomic <35199700+bitomic@users.noreply.github.com>
2023-05-04 00:06:32 -0600

    fix: use meaningful name in logs

--- src/framework/Task.ts
+++ src/framework/Task.ts
@@ -12,7 +12,12 @@ export abstract class Task extends Piece {
 	public override name = randomUUID()
 
 	protected get logger(): typeof logger {
-		if ( !this.#logger ) this.#logger = logger.child( { label: this.name } )
+		if ( !this.#logger ) {
+			const directory = this.location.directories.at( -1 ) ?? 'unknown'
+			const name = this.location.name.replace( /\.(js|cjs|mjs)/, '' )
+			const label = `${ directory }/${ name }`
+			this.#logger = logger.child( { label } )
+		}
 		return this.#logger
 	}
 
