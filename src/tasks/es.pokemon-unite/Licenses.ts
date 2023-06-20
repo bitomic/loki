a5dd99665a59d3bfb5db9e9e1b5d904eb8ba85b1
bitomic <35199700+bitomic@users.noreply.github.com>
2023-06-20 14:30:56 -0600

    fix: get stats and data from unite-db

--- src/tasks/es.pokemon-unite/Licenses.ts
+++ src/tasks/es.pokemon-unite/Licenses.ts
@@ -1,27 +1,25 @@
+import type { ArrayValidator } from '@sapphire/shapeshift'
 import { format } from 'lua-json'
-import type { HTMLElement } from 'node-html-parser'
 import type { JobsOptions } from 'bullmq'
-import { parse } from 'node-html-parser'
 import { request } from 'undici'
+import { s } from '@sapphire/shapeshift'
 import { Task } from '../../framework'
 import { Time } from '@sapphire/duration'
 
 interface IBasicData {
 	attackStyle: string
 	difficulty: string
-	isAlternativeForm: boolean
 	name: string
-	rol: string // to not break stuff already depending on this property
 	role: string
 	style: string
 }
 
 interface IStatsLevel {
-	attack: string
-	defense: string
-	hp: string
-	specialAttack: string
-	specialDefense: string
+	attack: number
+	defense: number
+	hp: number
+	specialAttack: number
+	specialDefense: number
 }
 
 interface IAllStats {
@@ -29,6 +27,28 @@ interface IAllStats {
 }
 
 export class UserTask extends Task {
+	public static readonly STAT = s.object( {
+		level: s.object( {
+			attack: s.number,
+			defense: s.number,
+			hp: s.number,
+			sp_attack: s.number,
+			sp_defense: s.number
+		} ).array,
+		name: s.string
+	} ).ignore
+
+	public static readonly POKEMON = s.object( {
+		damage_type: s.enum( ...[ 'Physical', 'Special' ] as const ),
+		display_name: s.string,
+		name: s.string,
+		tags: s.object( {
+			difficulty: s.enum( ...[ 'Novice', 'Intermediate', 'Expert' ] as const ),
+			range: s.enum( ...[ 'Melee', 'Ranged' ] as const ),
+			role: s.enum( ...[ 'Speedster', 'All-Rounder', 'Defender', 'Supporter', 'Attacker' ] as const )
+		} )
+	} ).ignore
+
 	public override jobOptions: JobsOptions = {
 		repeat: {
 			every: Time.Day
@@ -36,271 +56,88 @@ export class UserTask extends Task {
 	}
 
 	public async run(): Promise<void> {
-		const links = await this.getLinks()
-		if ( !links ) return
+		const stats = await UserTask.getJson( 'https://unite-db.com/stats.json', UserTask.STAT.array )
+		const pokemon = await UserTask.getJson( 'https://unite-db.com/pokemon.json', UserTask.POKEMON.array )
 
-		const data: {
-			[ key: string ]: IBasicData & {
-				stats: Record<string, Partial<IStatsLevel>>
-			}
-		} = {}
-		for ( const link of links ) {
-			const { body } = await request( link )
-			const parsed = parse( await body.text() )
-			const basicData = this.getBasicData( parsed )
-			const stats = await this.getStats( basicData.at( 0 )?.name )
+		const data: Record<string, IBasicData & IAllStats> = {}
 
-			for ( let i = 0; i < basicData.length; i++ ) {
-				const basicDataItem = basicData[ i ]
-				const statsItem = stats[ i ]
-				if ( !basicDataItem || !statsItem ) {
-					this.logger.warn( `There were found different basic data items and stats items for ${ link }` )
-					break
-				}
-				data[ basicDataItem.name ] = {
-					...basicDataItem,
-					...statsItem
-				}
-			}
+		const NAME_REPLACEMENTS: Record<string, string> = {
+			'Ninetales': 'Ninetales de Alola'
+		}
+		const ATTACK_STYLES = {
+			Physical: 'Ataque',
+			Special: 'Ataque especial'
+		}
+		const DIFFICULTIES = {
+			Expert: 'Alta',
+			Intermediate: 'Media',
+			Novice: 'Baja'
+		}
+		const RANGES = {
+			Melee: 'Corto alcance',
+			Ranged: 'Largo alcance'
+		}
+		const ROLES = {
+			'All-Rounder': 'Equilibrado',
+			Attacker: 'Ofensivo',
+			Defender: 'Defensivo',
+			Speedster: 'Ágil',
+			Supporter: 'Auxiliar'
 		}
 
-		// Is this necessary? Of course no!
-		const keys = [ ...Object.keys( data ) ].sort()
-		const sortedData: typeof data = {}
-		// @ts-expect-error - ignore
-		for ( const key of keys ) sortedData[ key ] = data[ key ]
+		for ( const key in stats ) {
+			const stat = stats[ key ]
+			const item = pokemon[ key ]
+			if ( !stat || !item ) {
+				this.logger.error( 'Missing data.' )
+				continue
+			}
+
+			const name = NAME_REPLACEMENTS[ item.display_name ] ?? item.display_name
+
+			data[ name ] = {
+				attackStyle: ATTACK_STYLES[ item.damage_type ],
+				difficulty: DIFFICULTIES[ item.tags.difficulty ],
+				name,
+				role: ROLES[ item.tags.role ],
+				stats: Object.fromEntries( Object.entries( stat.level ).map( ( [ idx, item ] ) => [
+					`${ parseInt( idx, 10 ) + 1 }`,
+					{
+						attack: item.attack,
+						defense: item.defense,
+						hp: item.hp,
+						specialAttack: item.sp_attack,
+						specialDefense: item.sp_defense
+					}
+				] ) ),
+				style: RANGES[ item.tags.range ]
+			}
+		}
 
 		const wiki = UserTask.getFandomWiki( 'es.pokemon-unite' )
 		const bot = await UserTask.getBot( wiki )
 		await bot.edit( {
 			bot: true,
-			text: format( sortedData ),
+			text: format( data ),
 			title: 'Module:Pokémon/datos'
 		} )
 	}
 
-	protected getBasicData( parsed: HTMLElement ): IBasicData[] {
-		const result = parsed.querySelector( '.tab' )?.querySelectorAll( '.fooinfo' ) // the main table where the image is
-			.map( ( i, idx ) => {
-				const lastColumn: Array<string | undefined> = i.nextElementSibling.nextElementSibling.querySelectorAll( 'tr' ).map( i => i.querySelectorAll( 'td' ).pop() )
-					.map( i => i?.innerText )
-				const [ difficulty, style, role, attackStyle ] = lastColumn
-				return {
-					attackStyle: this.normalizeAttackStyle( attackStyle ),
-					difficulty: this.normalizeDifficulty( difficulty ),
-					isAlternativeForm: idx !== 0,
-					name: this.normalizeName( i.parentNode.previousElementSibling.previousElementSibling.innerText ),
-					rol: this.normalizeRole( role ),
-					role: this.normalizeRole( role ),
-					style: this.normalizeStyle( style )
-				}
-			} ) as IBasicData[]
-
-		if ( result[ 0 ]?.name === 'Scizor' ) {
-			result.push( {
-				...result[ 0 ],
-				name: 'Scyther'
-			} )
-		}
-
-		return result
-	}
-
-	protected async getStats( name: string | undefined ): Promise<IAllStats[]> {
-		if ( !name ) return []
-
-		if ( name === 'Ninetales de Alola' ) name = 'ninetales'
-		else if ( name === 'Mr. Mime' ) name = 'mrmime'
-
-		const url = `https://www.pokexperto.net/index2.php?seccion=pokemonunite/${ name.toLowerCase() }`
+	protected static async getJson<T extends ArrayValidator<unknown[]>>( url: string, validator: T ): Promise<Record<string, ReturnType<T[ 'parse' ]>[ number ]>> {
 		const { body } = await request( url )
+		const data = validator.parse( await body.json() )
+		type Collection = Record<string, ReturnType<T[ 'parse' ]>[ number ]>
 
-		const parsed = parse( await body.text() )
-		const statsTable = parsed.querySelectorAll( 'th' ).find( i => i.innerText === 'Ataque Especial' )?.parentNode.parentNode.parentNode
-		const trs = statsTable?.querySelectorAll( 'tr' )
-		trs?.shift()
-
-		if ( !trs ) return []
-		else if ( name === 'Aegislash' ) return this.getAegislashStats( trs )
-		else if ( name === 'Hoopa' ) return this.getHoopaStats( trs )
-		else if ( name === 'Scizor' ) return this.getScizorStats( trs )
-
-		const allStats: IAllStats = { stats: {} }
-		for ( const tr of trs ) {
-			const level = tr.querySelector( 'th' )?.innerText.match( /(\d+)/ )?.at( 0 )
-			if ( !level ) continue
-
-			allStats.stats[ level ] = this.getRowStats( tr )
+		const reshaped = {} as Collection
+		for ( const item of data ) {
+			if ( item && typeof item === 'object' && 'name' in item ) {
+				const { name } = item
+				if ( typeof name === 'string' ) {
+					reshaped[ name ] = item
+				}
+			}
 		}
 
-		return [ allStats ]
-	}
-
-	protected getRowStats( tr: HTMLElement ): Partial<IStatsLevel> {
-		const levelStats: Partial<IStatsLevel> = {}
-
-		const tds = tr.querySelectorAll( 'td' ).map( i => i.innerText )
-		const [ hp, attack, defense, specialAttack, specialDefense ] = tds
-		if ( hp ) levelStats.hp = hp
-		if ( attack ) levelStats.attack = attack
-		if ( defense ) levelStats.defense = defense
-		if ( specialAttack ) levelStats.specialAttack = specialAttack
-		if ( specialDefense ) levelStats.specialDefense = specialDefense
-
-		return levelStats
-	}
-
-	protected getMultipleRowsStats( trs: HTMLElement[], from: number, to: number ): Array<Partial<IStatsLevel>> {
-		const stats: Array<Partial<IStatsLevel>> = []
-		for ( let i = from; i <= to; i++ ) {
-			const tr = trs.at( i )
-			if ( !tr ) continue
-			stats.push( this.getRowStats( tr ) )
-		}
-		return stats
-	}
-
-	protected getAegislashStats( trs: HTMLElement[] ): IAllStats[] {
-		const allStats: IAllStats[] = []
-
-		const baseStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 0, 5 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 1 }` ] = item
-			return list
-		}, baseStats )
-
-		const shieldStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 7, 15 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 7 }` ] = item
-			return list
-		}, shieldStats )
-
-		const bladeStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 17, 25 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 7 }` ] = item
-			return list
-		}, bladeStats )
-
-		allStats.push(
-			{ stats: { ...baseStats, ...bladeStats } },
-			{ stats: { ...baseStats, ...shieldStats } }
-		)
-		return allStats
-	}
-
-	protected getHoopaStats( trs: HTMLElement[] ): IAllStats[] {
-		const allStats: IAllStats[] = []
-
-		const baseStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 0, 7 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 1 }` ] = item
-			return list
-		}, baseStats )
-
-		const boundStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 9, 15 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 9 }` ] = item
-			return list
-		}, boundStats )
-
-		const unboundStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 17, 23 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 9 }` ] = item
-			return list
-		}, unboundStats )
-
-		allStats.push(
-			{ stats: { ...baseStats, ...boundStats } },
-			{ stats: { ...baseStats, ...unboundStats } }
-		)
-
-		return allStats
-	}
-
-	protected getScizorStats( trs: HTMLElement[] ): IAllStats[] {
-		const allStats: IAllStats[] = []
-
-		const baseStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 0, 3 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 1 }` ] = item
-			return list
-		}, baseStats )
-
-		const boundStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 5, 15 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 5 }` ] = item
-			return list
-		}, boundStats )
-
-		const unboundStats: IAllStats[ 'stats' ] = {}
-		this.getMultipleRowsStats( trs, 17, 27 ).reduce( ( list, item, index ) => {
-			list[ `${ index + 5 }` ] = item
-			return list
-		}, unboundStats )
-
-		allStats.push(
-			{ stats: { ...baseStats, ...boundStats } },
-			{ stats: { ...baseStats, ...unboundStats } }
-		)
-
-		return allStats
-	}
-
-	protected async getLinks(): Promise<Set<string> | null> {
-		const { body } = await request( 'https://www.serebii.net/pokemonunite/pokemon.shtml' )
-		const links = parse( await body.text() ).querySelector( '.dextable' )
-			?.querySelectorAll( 'a' )
-		if ( !links || links.length === 0 ) {
-			this.logger.warn( 'Couldn\'t find any link in Serebii.' )
-			return null
-		}
-
-		return new Set( links.map( i => {
-			const href = i.getAttribute( 'href' ) ?? ''
-			const url = new URL( href, 'https://www.serebii.net' )
-			return `${ url }` // eslint-disable-line @typescript-eslint/restrict-template-expressions
-		} ) )
-	}
-
-	protected normalizeAttackStyle( name?: string ): string {
-		return name === 'Physical Attacker' ? 'Ataque' : 'Ataque especial'
-	}
-
-	protected normalizeDifficulty( name?: string ): string {
-		if ( name === 'Novice' ) {
-			return 'Baja'
-		} else if ( name === 'Intermediate' ) {
-			return 'Media'
-		}
-		return 'Alta'
-	}
-
-	protected normalizeName( name: string ): string {
-		if ( name.startsWith( 'Alolan' ) ) {
-			return `${ name.split( / /g ).pop() } de Alola` // eslint-disable-line @typescript-eslint/restrict-template-expressions
-		} else if ( name === 'Shield Forme' ) {
-			return 'Aegislash (Forma Escudo)'
-		} else if ( name === 'Hoopa Unbound' ) {
-			return 'Hoopa (Desatado)'
-		}
-
-		return name
-	}
-
-	protected normalizeRole( name?: string ): string {
-		if ( name === 'Attacker' ) {
-			return 'Ofensivo'
-		} else if ( name === 'All-Rounder' ) {
-			return 'Equilibrado'
-		} else if ( name === 'Defender' ) {
-			return 'Defensivo'
-		} else if ( name === 'Speedster' ) {
-			return 'Ágil'
-		}
-		return 'Auxiliar'
-	}
-
-	protected normalizeStyle( name?: string ): string {
-		return name === 'Ranged' ? 'Largo alcance' : 'Corto alcance'
+		return reshaped
 	}
 }
