d3cf0b67a44459db23879797037f923541ba499d
bitomic <35199700+bitomic@users.noreply.github.com>
2023-06-23 15:30:21 -0600

    fix: add more data to moves, why not

--- src/tasks/es.pokemon-unite/Moves.ts
+++ src/tasks/es.pokemon-unite/Moves.ts
@@ -5,7 +5,7 @@ import { s } from '@sapphire/shapeshift'
 import { Task } from '../../framework'
 import { Time } from '@sapphire/duration'
 
-interface MoveData {
+interface MoveStats {
 	base: number
 	dmg_type: string
 	label: string
@@ -15,23 +15,28 @@ interface MoveData {
 
 interface MovesModule {
 	[ pokemon: string ]: {
-		[ move: string ]: MoveData[]
+		[ move: string ]: {
+			cooldown?: number
+			stats?: MoveStats[]
+			type?: string
+		}
 	}
-}
-
-const NAME_CONVERSIONS: Record<string, string> = {
-	Ninetales: 'Ninetales de Alola'
 }
 
 export class UserTask extends Task {
 	protected static readonly validator = s.object( {
-		display_name: s.string.transform( n => NAME_CONVERSIONS[ n ] ?? n ),
+		display_name: s.string,
 		skills: s.object( {
+			cd: s.string.or( s.number ).optional,
 			name: s.string,
 			rsb: s.object( {} ).passthrough.optional,
+			type: s.string.optional,
 			upgrades: s.object( {
+				cd: s.string.or( s.number ).optional,
+				cd1: s.string.or( s.number ).optional,
 				name: s.string,
-				rsb: s.object( {} ).passthrough
+				rsb: s.object( {} ).passthrough,
+				type: s.string
 			} ).passthrough.array.optional
 		} ).passthrough.array
 	} ).ignore.array
@@ -51,16 +56,33 @@ export class UserTask extends Task {
 
 			for ( const skill of skills ) {
 				if ( !skill.rsb ) continue
-				const moves = this.addMoves( skill.rsb )
-				if ( moves ) {
-					pokemon[ skill.name ] = moves
+				const move: MovesModule[ string ][ string ] = {}
+				if ( skill.type ) move.type = skill.type
+
+				const cooldown = skill.cd
+				if ( cooldown && ( typeof cooldown !== 'string' || cooldown.length > 0 ) ) {
+					move.cooldown = typeof cooldown === 'number' ? cooldown : parseInt( cooldown, 10 )
+				}
+				pokemon[ skill.name ] = move
+				const stats = this.addMoves( skill.rsb )
+				if ( stats ) {
+					move.stats = stats
 				}
 
 				if ( !skill.upgrades ) continue
 				for ( const upgrade of skill.upgrades ) {
-					const moves = this.addMoves( upgrade.rsb )
-					if ( moves ) {
-						pokemon[ upgrade.name ] = moves
+					const move: MovesModule[ string ][ string ] = {
+						type: upgrade.type
+					}
+					const cooldown = upgrade.cd ?? upgrade.cd1
+					if ( cooldown ) {
+						move.cooldown = typeof cooldown === 'number' ? cooldown : parseInt( cooldown, 10 )
+					}
+					pokemon[ upgrade.name ] = move
+
+					const stats = this.addMoves( upgrade.rsb )
+					if ( stats ) {
+						move.stats = stats
 					}
 				}
 			}
@@ -68,6 +90,8 @@ export class UserTask extends Task {
 			if ( Object.keys( pokemon ).length ) {
 				module[ display_name ] = pokemon
 			}
+
+			break
 		}
 
 		const wiki = UserTask.getFandomWiki( 'es.pokemon-unite' )
@@ -79,8 +103,8 @@ export class UserTask extends Task {
 		} )
 	}
 
-	protected addMoves( rsb: Record<string, unknown> ): MoveData[] | null {
-		const moves: MoveData[] = []
+	protected addMoves( rsb: Record<string, unknown> ): MoveStats[] | null {
+		const moves: MoveStats[] = []
 		for ( let i = 0; i <= 5; i++ ) {
 			const prefix = i === 0 ? '' : `add${ i }_`
 			const move = this.getMoveFields( rsb, prefix )
@@ -91,13 +115,13 @@ export class UserTask extends Task {
 		return moves.length ? moves : null
 	}
 
-	protected getMoveFields<T extends string>( move: unknown, prefix: T ): MovesModule[ string ][ string ][ number ] | null {
+	protected getMoveFields<T extends string>( move: unknown, prefix: T ): MoveStats | null {
 		const validator = s.object( {
 			[ `${ prefix }label` ]: s.string.lengthGreaterThan( 0 ),
-			[ `${ prefix }ratio` ]: s.string.lengthGreaterThan( 0 ),
-			[ `${ prefix }dmg_type` ]: s.string.lengthGreaterThan( 0 ),
-			[ `${ prefix }slider` ]: s.string.lengthGreaterThan( 0 ),
-			[ `${ prefix }base` ]: s.string.lengthGreaterThan( 0 )
+			[ `${ prefix }ratio` ]: s.string.transform( v => v.length ? v : '0' ),
+			[ `${ prefix }dmg_type` ]: s.string.optional,
+			[ `${ prefix }slider` ]: s.string.transform( v => v.length ? v : '0' ),
+			[ `${ prefix }base` ]: s.string.transform( v => v.length ? v : '0' )
 		} )
 		const parsed = validator.run( move )
 		if ( parsed.isErr() ) return null
