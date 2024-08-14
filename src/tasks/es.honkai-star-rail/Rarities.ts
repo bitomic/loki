24d4930c552c890a710fe639b2b94c83270a567b
bitomic <35199700+bitomic@users.noreply.github.com>
2024-08-14 15:04:26 -0600

    fix: hsr rare items, and negative rare items

--- src/tasks/es.honkai-star-rail/Rarities.ts
+++ src/tasks/es.honkai-star-rail/Rarities.ts
@@ -13,16 +13,22 @@ export class UserTask extends WikiTask {
 	public async run(): Promise<void> {
 		const wiki = UserTask.getFandomWiki( 'es.honkai-star-rail' )
 		const bot = await UserTask.getBot( wiki )
-		const itemTypes = [ 'Personajes', 'Recursos', 'Conos de luz' ]
+		const itemTypes = [ 'Personajes', 'Recursos', 'Conos de luz', 'Objetos raros' ]
 		const rarities: Record<string, number> = {}
 		for ( const itemType of itemTypes ) {
 			for ( let i = 1; i <= 5; i++ ) {
-				const category = `${ itemType } de ${ i } estrella${ i === 1 ? '' : 's' }`
+				const category = `${ itemType } de ${ i } estrellas`
 				const pages = await this.getPagesInCategory( wiki, category )
 				for ( const page of pages ) {
 					rarities[ page ] = i
 				}
 			}
+		}
+
+		// Objetos raros
+		const pages = await this.getPagesInCategory( wiki, 'Objetos raros' )
+		for ( const page of pages ) {
+			rarities[ page ] ??= -1
 		}
 
 		this.logger.info( 'Updating rarities...' )
